b0ca766bc3886c3066415b60108eee07d2621ec8
vikas <rohit.excel2011@gmail.com>
2018-07-18 16:18:24 +0530

    menu responsive design done

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,8 +6,9 @@ import '../styles/menu.css';
 
 class Menu extends Component {
     state = {
-        openRequest: false,
-        scrolledClass: ''
+        isRequestOpen: false,
+        scrolledClass: '',
+        isMenuOpen: false
     }
     componentDidMount(){
         window.addEventListener('scroll', (e)=>this.handleScroll(e));
@@ -16,10 +17,9 @@ class Menu extends Component {
         window.removeEventListener('scroll', this.handleScroll())
     }
     handleScroll = (e) =>{
-        console.log('sssssssssssssss', e, window.scrollY);
         if(window.scrollY > 200){
             this.setState({
-                scrolledClass: 'isScrolled'
+                scrolledClass: ' isScrolled'
             });
         }else{
             this.setState({
@@ -27,21 +27,21 @@ class Menu extends Component {
             })
         }
     }
+    toggleMenu = () =>{
+        let {isMenuOpen} = this.state;
+        this.setState({isMenuOpen: !isMenuOpen});
+    }
   render() {
+      let {scrolledClass, isMenuOpen} = this.state;
     return (
         <div className="js-nav__wrapper">
-        <div className="menu-icon">         
-            <div className="bar"></div>         
-            <div className="bar"></div>         
-            <div className="bar"></div>       
-        </div>
-        <nav className={"js-nav--main " + this.state.scrolledClass}>
+        <nav className={"js-nav--main " + scrolledClass + (isMenuOpen ? ' is-menu-open':'')}>
             <div className="logo">           
                 <img src={logo}/>      
             </div>
             <div className="menu">
                 <a className="p-05">
-                    <h5 id="philosophy-link" className="menu-item">PHILOSOPHY</h5>
+                    <h5 id="philosophy-link" className="menu-item m-t-3">PHILOSOPHY</h5>
                 </a>
                 <a className="p-05">
                     <h5 id="product-link" className="menu-item">PRODUCT</h5>
@@ -52,10 +52,15 @@ class Menu extends Component {
                 <a id="login" className="p-05">
                     <h5 className="menu-item">SIGN IN</h5>
                 </a>
-                <Button onClick={()=>this.setState({openRequest:true})} label="REQUEST A DEMO" />
+                <Button _classname="request-a-demo-btn" onClick={()=>this.setState({isRequestOpen:true})} label="REQUEST A DEMO" />
+            </div>
+            <div className="menu-icon" onClick={this.toggleMenu}>         
+                <div className="bar"></div>         
+                <div className="bar"></div>         
+                <div className="bar"></div>       
             </div>
         </nav>
-            <RequestDemoModal openRequest={this.state.openRequest} close={()=>this.setState({openRequest:false})}/>
+            <RequestDemoModal openRequest={this.state.isRequestOpen} close={()=>this.setState({isRequestOpen:false})}/>
         </div>
     );
   }
